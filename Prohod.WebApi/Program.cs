ce51da42f5c5be89b14e680335b8e12d209036b1
jexinox <jexinox@gmail.com>
2023-06-04 12:17:53 +0500

    using swagger anyway

--- Prohod.WebApi/Program.cs
+++ Prohod.WebApi/Program.cs
@@ -23,12 +23,8 @@ builder.Services
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
